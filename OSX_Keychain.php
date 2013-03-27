a4271035d44feb568f90c070e8b3555474aa1c24
Matt <matt@sailthru.com>
2013-03-27 17:53:38 -0400

    checking exec completion for finding stuff

--- OSX_Keychain.php
+++ OSX_Keychain.php
@@ -9,48 +9,48 @@ class OSX_Keychain {
     public static function getInternetUsername($key) {
         $com = "security find-internet-password -s $key | grep \"acct\" | cut -d '\"' -f 4";
         exec($com,$out,$err);
-        return $out[0];
+        return (!$err) ? $out[0] : "username not found";
     }
 
     public static function getInternetPassword($key) {
         $com = "security 2>&1 > /dev/null find-internet-password -gs $key | cut -d '\"' -f 2";
         exec($com,$out,$err);
-        return $out[0];
+        return (!$err) ? $out[0] : "password not found";
     }
 
     public static function setInternetPassword($key, $account, $password) {
         $com = "security 2>&1 > /dev/null add-internet-password -a $account -s $key -w $password";
         exec($com, $out, $err);
-        return ($err != 0) ? $out : true;
+        return ($err) ? $out : true;
     }
 
     public static function hasInternetPassword($key) {
         $com = "security 2>&1 > /dev/null find-internet-password -gs $key";
         exec($com, $out, $err);
-        return $err == 0;
+        return !$err;
     }
 
     public static function setGenericPassword($account, $service, $pass) {
         $com = "security add-generic-password -a $account -s $service -w $pass";
         exec($com,$out,$err);
-        return ($err !=0) ? $out : true;
+        return (!$err) ? $out : true;
     }
 
     public static function findGenericPassword($account, $service) {
         $com = "security 2>&1 > /dev/null find-generic-password -a $account -s $service -g | cut -d '\"' -f 2";
         exec($com,$out,$err);
-        return $out[0];
+        return (!$err) ? $out[0] : "password not found";
     }
 
     public static function deleteGenericPassword($account, $service) {
         $com = "security delete-generic-password -a $account -s $service";
         exec($com,$out,$err);
-        return $err == 0;
+        return !$err;
     }
 
     public static function hasGenericPassword($account, $service) {
         $com = "security 2>&1 > /dev/null find-generic-password -a $account -s $service -g";
         exec($com,$out,$err);
-        return $err == 0;
+        return !$err;
     }
 }
